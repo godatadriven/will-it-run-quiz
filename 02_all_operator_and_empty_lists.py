086f64509d8d1481aacd9aa219d0dd554a4200fe
Jeroen Overschie <jeroen.overschie@tmnl.nl>
2022-09-20 20:19:21 +0200

    `all` operator with empty lists

---
+++ 02_all_operator_and_empty_lists.py
@@ -0,0 +1,15 @@
+from rich.traceback import install
+install(show_locals=True)
+
+def will_it_run():
+    # hey, do you know the built-in `all` function? it's pretty easy
+    assert all([True, True, True]) is True
+    assert all([True, True, False]) is False
+
+    # or, is it? let's check below
+    assert all([]) is True
+    assert all([[]]) is False
+    assert all([[[]]]) is True
+
+if __name__ == "__main__":
+    will_it_run()
