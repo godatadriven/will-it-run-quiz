1faa796c4e607b3b744e627bef8bb97a5453091a
Daniel Willemsen <danielwillemsen@live.nl>
2022-09-20 21:01:30 +0200

    added 11

---
+++ 11_key_type_conversion.py
@@ -0,0 +1,16 @@
+# from rich.traceback import install
+
+# install(show_locals=True)
+
+
+def will_it_run():
+    d = {1: 'a', True: 'b', 1.0: 'c'}
+
+    assert 'c' in d.values()
+    assert 'b' in d.values()
+    assert 'a' in d.values()
+    print("🎉🎉🎉 It all ran!!! 🎉🎉🎉")
+
+
+if __name__ == "__main__":
+    will_it_run()
