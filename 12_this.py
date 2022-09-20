747558c22d3b3a31741cffed6b36a45d50419c9a
Rens Dimmendaal <rens@dimmendaal.com>
2022-09-20 21:00:15 +0200

    add this

---
+++ 12_this.py
@@ -0,0 +1,22 @@
+
+from rich.traceback import install
+
+install(show_locals=True)
+
+def will_it_run():
+    import this
+    print("-"*20)
+    love = this
+
+    assert this is love
+    assert love is True
+    assert love is False
+    assert love is not True or False
+    assert love is not True or False
+    assert love is love
+
+    print("🎉🎉🎉 It all ran!!! 🎉🎉🎉")
+
+
+if __name__ == "__main__":
+    will_it_run()
