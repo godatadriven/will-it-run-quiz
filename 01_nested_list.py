3c75d9fd429e14975abd3cfc1caa9b44e3a252e0
Rens Dimmendaal <rens@dimmendaal.com>
2022-09-20 20:10:14 +0200

    add first question

---
+++ 01_nested_list.py
@@ -0,0 +1,14 @@
+from rich.traceback import install
+install(show_locals=True)
+
+def will_it_run():
+    row = ['']*3  #  one row of three columns
+    field = [row]*3 # field of three rows
+    field[0][0] = 'X'  # fill top left row with an X
+
+    expected = [["X", "", ""],["","",""],["","",""]]
+
+    assert field == expected
+
+if __name__ == "__main__":
+    will_it_run()
